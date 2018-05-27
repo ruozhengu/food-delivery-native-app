d52721797623e297bceb564963c9791e992b83f8
Ali Saeed <asalpha01@gmail.com>
2018-05-27 16:56:29 -0400

    homescreen udpate

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -11,6 +11,7 @@ import {
 import { WebBrowser } from 'expo';
 
 import { MonoText } from '../components/StyledText';
+import LaunchScreen from './LaunchScreen'
 
 export default class HomeScreen extends React.Component {
   static navigationOptions = {
@@ -18,50 +19,51 @@ export default class HomeScreen extends React.Component {
   };
 
   render() {
-    return (
-      <View style={styles.container}>
-        <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-          <View style={styles.welcomeContainer}>
-            <Image
-              source={
-                __DEV__
-                  ? require('../assets/images/robot-dev.png')
-                  : require('../assets/images/robot-prod.png')
-              }
-              style={styles.welcomeImage}
-            />
-          </View>
+    return <LaunchScreen />;
+    // return (
+    //   <View style={styles.container}>
+    //     <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
+    //       <View style={styles.welcomeContainer}>
+    //         <Image
+    //           source={
+    //             __DEV__
+    //               ? require('../assets/images/robot-dev.png')
+    //               : require('../assets/images/robot-prod.png')
+    //           }
+    //           style={styles.welcomeImage}
+    //         />
+    //       </View>
 
-          <View style={styles.getStartedContainer}>
-            {this._maybeRenderDevelopmentModeWarning()}
+    //       <View style={styles.getStartedContainer}>
+    //         {this._maybeRenderDevelopmentModeWarning()}
 
-            <Text style={styles.getStartedText}>Get started by opening</Text>
+    //         <Text style={styles.getStartedText}>Get started by opening</Text>
 
-            <View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-              <MonoText style={styles.codeHighlightText}>screens/HomeScreen.js</MonoText>
-            </View>
+    //         <View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
+    //           <MonoText style={styles.codeHighlightText}>screens/HomeScreen.js</MonoText>
+    //         </View>
 
-            <Text style={styles.getStartedText}>
-              Change this text and your app will automatically reload.
-            </Text>
-          </View>
+    //         <Text style={styles.getStartedText}>
+    //           TESTINNNNGGGGGG !!!!!! Change this text and your app will automatically reload.
+    //         </Text>
+    //       </View>
 
-          <View style={styles.helpContainer}>
-            <TouchableOpacity onPress={this._handleHelpPress} style={styles.helpLink}>
-              <Text style={styles.helpLinkText}>Help, it didn’t automatically reload!</Text>
-            </TouchableOpacity>
-          </View>
-        </ScrollView>
+    //       <View style={styles.helpContainer}>
+    //         <TouchableOpacity onPress={this._handleHelpPress} style={styles.helpLink}>
+    //           <Text style={styles.helpLinkText}>Help, it didn’t automatically reload!</Text>
+    //         </TouchableOpacity>
+    //       </View>
+    //     </ScrollView>
 
-        <View style={styles.tabBarInfoContainer}>
-          <Text style={styles.tabBarInfoText}>This is a tab bar. You can edit it in:</Text>
+    //     <View style={styles.tabBarInfoContainer}>
+    //       <Text style={styles.tabBarInfoText}>TESTTTTTINNGGGGG !!!This is a tab bar. You can edit it in:</Text>
 
-          <View style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-            <MonoText style={styles.codeHighlightText}>navigation/MainTabNavigator.js</MonoText>
-          </View>
-        </View>
-      </View>
-    );
+    //       <View style={[styles.codeHighlightContainer, styles.navigationFilename]}>
+    //         <MonoText style={styles.codeHighlightText}>navigation/MainTabNavigator.js</MonoText>
+    //       </View>
+    //     </View>
+    //   </View>
+    // );
   }
 
   _maybeRenderDevelopmentModeWarning() {
