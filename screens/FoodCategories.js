7746a00cb2b0992bf5ff122815fd17f66f5af5a0
Ali Saeed <asalpha01@gmail.com>
2018-06-03 18:32:55 -0400

    Categories page updated

--- screens/FoodCategories.js
+++ screens/FoodCategories.js
@@ -1,122 +1,84 @@
-import React from 'react';
-import {
-  Image,
-  Platform,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-} from 'react-native';
-import { WebBrowser } from 'expo';
+import React, { Component } from 'react';
+import { ListView, Image, Text, View, StyleSheet } from 'react-native';
+import { Constants } from 'expo';
 
-import { MonoText } from '../components/StyledText';
+// You can import from local files
+import AssetExample from './components/AssetExample';
 
-export default class Resturants extends React.Component {
-  static navigationOptions = {
-    header: null,
-  };
+// or any pure javascript modules available in npm
+import { Card } from 'react-native-elements'; // Version can be specified in package.json
 
+let data = [{name:"Category 1"}, {name:'Category 2'}, {name:'Category 3'}, {name:'Category 4'}, {name:'Category 5'}, {name:'Category 6'}, {name:'Category 7'}, {name:'Category 8'}, {name:'Category 9'}, {name:'Category 10'}, {name:'Category 11'}, {name:'Category 12'}, {name:'Category 13'}, {name:'Category 14'}, {name:'Category 15'}, {name:'Category 16'}, {name:'Category 17'}, {name:'Category 18'}, {name:'Category 19'}, {name:'Category 20'} ];
+
+export default class FoodCategories extends Component {
+  constructor(props) {
+    super(props);
+
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      dataSource: ds.cloneWithRows(data),
+    };
+  }
   render() {
     return (
-      <View style={styles.container}>
-        <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-
-          <View style={styles.titleContainer}>
-            <Text style={styles.titleText}>Categories</Text>
-          </View>
-          <View style={styles.getStartedContainer}>
-            <Text style={styles.getStartedText}>
-              Categories of food go here.
-            </Text>
-          </View>
-
-        </ScrollView>
-      </View>
+      <ListView
+        style={styles.container}
+        dataSource={this.state.dataSource}
+        renderRow={(data) => <Row {...data} />}
+        renderSeparator={(sectionId, rowId) => <View key={rowId} style={styles.separator} />}
+        renderHeader={() => <Header />}
+      />
     );
   }
-
 }
+
+const Header = (props) => (
+  <View style={styles.headerContainer}>
+    <Text style={styles.text}>Categories</Text>
+  </View>
+  );
+
+const Row = (props) => (
+  <View style={styles.rowContainer}>
+    <Image source={{ uri: 'https://images.pexels.com/photos/89432/pexels-photo-89432.jpeg?h=350&dpr=2&auto=compress&cs=tinysrgb'}} style={styles.photo} />
+    <Text style={styles.text}>
+      {props.name}, {props.name}
+    </Text>
+  </View>
+);
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    marginTop: 20,
   },
-  contentContainer: {
-    paddingTop: 30,
-  },
-  getStartedContainer: {
-    flex : 1,
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-  titleContainer : {
-    flex : 1,
-    alignItems : 'center',
-    marginBottom : 10
-
-  },
-
-  titleText : {
-    fontSize : 30,
-    color : '#000',
-    textAlign : 'left'
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
+  rowContainer: {
+    flex: 1,
+    padding: 12,
+    flexDirection: 'row',
     alignItems: 'center',
   },
-  helpLink: {
-    paddingVertical: 15,
+  headerContainer: {
+    backgroundColor: 'red',
+    flex: 1,
+    padding: 12,
+    flexDirection: 'row',
+    alignItems: 'center',
   },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
+  text: {
+    marginLeft: 12,
+    fontSize: 16,
+  },
+  photo: {
+    height: 40,
+    width: 40,
+    borderRadius: 20,
+  },
+  separator: {
+    flex: 1,
+    height: StyleSheet.hairlineWidth,
+    backgroundColor: '#8E8E8E',
   },
 });
+
--- screens/Resturants.js
+++ screens/Resturants.js
@@ -7,115 +7,141 @@ import {
   Text,
   TouchableOpacity,
   View,
+  Row,
 } from 'react-native';
 import { WebBrowser } from 'expo';
 
-import { MonoText } from '../components/StyledText';
+import { Component } from 'react';
+import { Animated } from 'react-native';
 
 export default class Resturants extends React.Component {
-  static navigationOptions = {
-    header: null,
+  constructor() {
+    super();
+
+    this.state = { valueArray: [], disabled: false };
+
+    this.index = 0;
+
+    this.animatedValue = new Animated.Value(0);
+  }
+
+  addMore = () => {
+    this.animatedValue.setValue(0);
+
+    let newlyAddedValue = { index: this.index };
+
+    this.setState(
+      {
+        disabled: true,
+        valueArray: [...this.state.valueArray, newlyAddedValue],
+      },
+      () => {
+        Animated.timing(this.animatedValue, {
+          toValue: 1,
+          duration: 500,
+          useNativeDriver: true,
+        }).start(() => {
+          this.index = this.index + 1;
+          this.setState({ disabled: false });
+        });
+      }
+    );
   };
 
   render() {
+    const animationValue = this.animatedValue.interpolate({
+      inputRange: [0, 1],
+      outputRange: [-59, 0],
+    });
+
+    let newArray = this.state.valueArray.map((item, key) => {
+      if (key == this.index) {
+        return (
+          <Animated.View
+            key={key}
+            style={[
+              styles.viewHolder,
+              {
+                opacity: this.animatedValue,
+                transform: [{ translateY: animationValue }],
+              },
+            ]}>
+            <Text style={styles.text}>Row {item.index}</Text>
+          </Animated.View>
+        );
+      } else {
+        return (
+          <View key={key} style={styles.viewHolder}>
+            <Text style={styles.text}>Row {item.index}</Text>
+          </View>
+        );
+      }
+    });
+
     return (
       <View style={styles.container}>
-        <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-         
-
-          <View style={styles.titleContainer}>
-            <Text style={styles.titleText}>Resturants page</Text>
+        <ScrollView>
+          <View style={{ flex: 1, padding: 4 }}>
+            {newArray}
           </View>
-          <View style={styles.getStartedContainer}>
-            <Text style={styles.getStartedText}>
-              The list of resturants will go here.
-            </Text>
-          </View>
-
         </ScrollView>
+
+        <TouchableOpacity
+          activeOpacity={0.8}
+          style={styles.btn}
+          disabled={this.state.disabled}
+          onPress={this.addMore}>
+          <Image source={{uri: 'https://images.pexels.com/photos/89432/pexels-photo-89432.jpeg?h=350&dpr=2&auto=compress&cs=tinysrgb'}}
+          style={styles.btnImage} />
+        </TouchableOpacity>
       </View>
     );
   }
-
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-  contentContainer: {
-    paddingTop: 30,
-  },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-  titleContainer : {
-    alignItems : 'center',
-    marginBottom : 10
+const styles = StyleSheet.create(
+{
+    container:
+    {
+        flex: 1,
+        backgroundColor: '#eee',
+        justifyContent: 'center',
+        paddingTop: (Platform.OS == 'ios') ? 20 : 0
+    },
 
-  },
+    viewHolder:
+    {
+        height: 55,
+        backgroundColor: '#26A69A',
+        justifyContent: 'center',
+        alignItems: 'center',
+        margin: 4
+    },
 
-  titleText : {
-    fontSize : 30,
-    color : '#000',
-    textAlign : 'left'
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
-  },
+    text:
+    {
+        color: 'white',
+        fontSize: 25
+    },
+
+    btn:
+    {
+        position: 'absolute',
+        right: 25,
+        bottom: 25,
+        borderRadius: 30,
+        width: 60,
+        height: 60,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgba(0,0,0,0.7)',
+        padding: 15
+    },
+
+    btnImage:
+    {
+        resizeMode: 'contain',
+        width: '100%',
+        tintColor: 'white'
+    }
 });
