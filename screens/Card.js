dd5eefeff17ce66abf272116c8dcbbd66e3439db
Ali Saeed <asalpha01@gmail.com>
2018-06-10 19:01:37 -0400

    categories side by side cards with sticky search bar ion top

--- screens/Card.js
+++ screens/Card.js
@@ -1,41 +1,209 @@
-
-
-import { Card, CardTitle, CardContent, CardAction, CardButton, CardImage } from 'react-native-material-cards'
-
 import React, { Component } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
-import { Constants } from 'expo';
+import {
+  Alert,
+  TouchableOpacity,
+  Dimensions,
+  Image,
+  ListView,
+  PixelRatio,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+import ParallaxScrollView from 'react-native-parallax-scroll-view';
+import { Card } from 'react-native-elements'; // Version can be specified in package.json
+//cards original with row by row category
+
+let data = [
+        {name: 'Chinese', image: 'https://images.pexels.com/photos/89432/pexels-photo-89432.jpeg?h=350&dpr=2&auto=compress&cs=tinysrgb'},
+        {name: 'Indian', image: 'https://images.pexels.com/photos/89432/pexels-photo-89432.jpeg?h=350&dpr=2&auto=compress&cs=tinysrgb'},
+        {name: 'Italian', image: 'https://images.pexels.com/photos/89432/pexels-photo-89432.jpeg?h=350&dpr=2&auto=compress&cs=tinysrgb'},
+        {name: 'French', image: 'https://images.pexels.com/photos/89432/pexels-photo-89432.jpeg?h=350&dpr=2&auto=compress&cs=tinysrgb'},
+        {name: 'Spicy', image: 'https://images.pexels.com/photos/89432/pexels-photo-89432.jpeg?h=350&dpr=2&auto=compress&cs=tinysrgb'},
+        {name: 'Polish', image: 'https://images.pexels.com/photos/89432/pexels-photo-89432.jpeg?h=350&dpr=2&auto=compress&cs=tinysrgb'},
+        {name: 'German', image: 'https://images.pexels.com/photos/89432/pexels-photo-89432.jpeg?h=350&dpr=2&auto=compress&cs=tinysrgb'},
+        {name: 'food', image: 'https://images.pexels.com/photos/89432/pexels-photo-89432.jpeg?h=350&dpr=2&auto=compress&cs=tinysrgb'},
+        {name: 'Category 9', image: 'https://images.pexels.com/photos/89432/pexels-photo-89432.jpeg?h=350&dpr=2&auto=compress&cs=tinysrgb'},
+        {name: 'Category 10', image: 'https://images.pexels.com/photos/89432/pexels-photo-89432.jpeg?h=350&dpr=2&auto=compress&cs=tinysrgb'},
+        {name: 'Category 11', image: 'https://images.pexels.com/photos/89432/pexels-photo-89432.jpeg?h=350&dpr=2&auto=compress&cs=tinysrgb'},
+        {name: 'Category 12', image: 'https://images.pexels.com/photos/89432/pexels-photo-89432.jpeg?h=350&dpr=2&auto=compress&cs=tinysrgb'},
+        {name: 'Category 13', image: 'https://images.pexels.com/photos/89432/pexels-photo-89432.jpeg?h=350&dpr=2&auto=compress&cs=tinysrgb'},
+        {name: 'Category 14', image: 'https://images.pexels.com/photos/89432/pexels-photo-89432.jpeg?h=350&dpr=2&auto=compress&cs=tinysrgb'},
+      ];
 
 
+class FoodCategories extends Component {
 
-export default class App extends Component {
+
+  static navigationOptions = {
+    header: null,
+  };
+
+  constructor(props) {
+    super(props);
+    this.state =  {
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (r1, r2) => r1 !== r2
+      }).cloneWithRows(data)
+    };
+  }
+
+
   render() {
+    const { onScroll = () => {} } = this.props;
     return (
-      <View style={styles.container}>
-        <Text style={styles.paragraph}>
-          Change code in the editor and watch it change on your phone!
-          Save to get a shareable url.
-        </Text>
-        <Card title="Local Modules">
-        </Card>
-      </View>
+      <ListView
+        ref="ListView"
+        style={styles.container}
+        dataSource={ this.state.dataSource }
+
+        renderRow={(rowData) => (
+          <TouchableOpacity onPress={()=>{Alert.alert(rowData.name)}}>
+          <View key={rowData} style={ styles.rowContainer }>
+              <Image source={{ uri: rowData.image}} style={styles.photo} />
+            <Text style={ styles.rowText }>
+              { rowData.name }
+            </Text>
+          </View>
+          </TouchableOpacity>
+         )}
+
+
+        renderSeparator={(sectionId, rowId) => <View key={rowId} style={styles.separator} />}
+
+
+        renderScrollComponent={props => (
+          <ParallaxScrollView
+            onScroll={onScroll}
+
+            headerBackgroundColor="#fff"
+            stickyHeaderHeight={ STICKY_HEADER_HEIGHT }
+            parallaxHeaderHeight={ PARALLAX_HEADER_HEIGHT }
+            backgroundSpeed={10}
+
+
+            renderBackground={() => (
+              <View key="background">
+                <Image source={{uri: 'https://i.pinimg.com/736x/7b/ff/9f/7bff9fcfe8d530c7326a57ba66479b81--restaurant-branding-restaurant-interiors.jpg',
+                                width: window.width,
+                                height: PARALLAX_HEADER_HEIGHT}}/>
+                <View style={{position: 'absolute',
+                              top: 0,
+                              width: window.width,
+                              backgroundColor: 'rgba(0,0,0,0.4)',
+                              height: PARALLAX_HEADER_HEIGHT}}/>
+              </View>
+            )}
+
+
+            renderForeground={() => (
+              <View key="parallax-header" style={ styles.parallaxHeader }>
+                <Text style={ styles.Title }>
+                  Categories
+                </Text>
+              </View>
+            )}
+
+
+            />
+        )}
+      />
     );
   }
 }
 
+const window = Dimensions.get('window');
+
+const AVATAR_SIZE = 120;
+const ROW_HEIGHT = 60;
+const PARALLAX_HEADER_HEIGHT = 150;
+const STICKY_HEADER_HEIGHT = 70;
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#ecf0f1',
+    backgroundColor: 'black',
   },
-  paragraph: {
-    margin: 24,
+  background: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: window.width,
+    height: PARALLAX_HEADER_HEIGHT
+  },
+  stickySection: {
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+
+  },
+  stickySectionText: {
+    color: 'white',
+    fontSize: 20,
+    margin: 10
+  },
+    photo: {
+    height: 40,
+    width: 40,
+    borderRadius: 20,
+  },
+  fixedSection: {
+    position: 'absolute',
+    bottom: 10,
+    right: 10
+  },
+  fixedSectionText: {
+    color: '#999',
+    fontSize: 20
+  },
+  parallaxHeader: {
+    alignItems: 'center',
+    flex: 1,
+    flexDirection: 'column',
+    paddingTop: 50
+  },
+  avatar: {
+    marginBottom: 10,
+    borderRadius: AVATAR_SIZE / 2
+  },
+  Title: {
+    color: 'white',
+    fontSize: 32,
+    paddingVertical: 5,
+  },
+  sectionTitleText: {
+    color: 'white',
     fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    color: '#34495e',
+    paddingVertical: 5
+  },
+  row: {
+    overflow: 'hidden',
+    paddingHorizontal: 10,
+    height: ROW_HEIGHT,
+    backgroundColor: 'white',
+    borderColor: '#ccc',
+    borderBottomWidth: 1,
+    justifyContent: 'center'
+  },
+  rowText: {
+    fontSize: 20,
+    marginLeft: 12,
+
+  },
+    rowContainer: {
+    flex: 1,
+    padding: 15,
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#F5F5F5',
+  },
+    separator: {
+    flex: 1,
+    height: StyleSheet.hairlineWidth,
+    backgroundColor: '#8E8E8E',
   },
 });
+
+export default FoodCategories;
--- screens/FoodCategories.js
+++ screens/FoodCategories.js
@@ -1,209 +1,234 @@
 import React, { Component } from 'react';
-import {
-  Alert,
-  TouchableOpacity,
-  Dimensions,
-  Image,
-  ListView,
-  PixelRatio,
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
 
-import ParallaxScrollView from 'react-native-parallax-scroll-view';
-import { Card } from 'react-native-elements'; // Version can be specified in package.json
+import { TouchableOpacity, Text, StyleSheet, View, ListView, TextInput, ActivityIndicator, Alert } from 'react-native';
+import { Icon, Card } from 'react-native-elements'; // Version can be specified in package.json
+
 
 
 let data = [
-        {name: 'Chinese', image: 'https://images.pexels.com/photos/89432/pexels-photo-89432.jpeg?h=350&dpr=2&auto=compress&cs=tinysrgb'},
-        {name: 'Indian', image: 'https://images.pexels.com/photos/89432/pexels-photo-89432.jpeg?h=350&dpr=2&auto=compress&cs=tinysrgb'},
-        {name: 'Italian', image: 'https://images.pexels.com/photos/89432/pexels-photo-89432.jpeg?h=350&dpr=2&auto=compress&cs=tinysrgb'},
-        {name: 'French', image: 'https://images.pexels.com/photos/89432/pexels-photo-89432.jpeg?h=350&dpr=2&auto=compress&cs=tinysrgb'},
-        {name: 'Spicy', image: 'https://images.pexels.com/photos/89432/pexels-photo-89432.jpeg?h=350&dpr=2&auto=compress&cs=tinysrgb'},
-        {name: 'Polish', image: 'https://images.pexels.com/photos/89432/pexels-photo-89432.jpeg?h=350&dpr=2&auto=compress&cs=tinysrgb'},
-        {name: 'German', image: 'https://images.pexels.com/photos/89432/pexels-photo-89432.jpeg?h=350&dpr=2&auto=compress&cs=tinysrgb'},
-        {name: 'food', image: 'https://images.pexels.com/photos/89432/pexels-photo-89432.jpeg?h=350&dpr=2&auto=compress&cs=tinysrgb'},
-        {name: 'Category 9', image: 'https://images.pexels.com/photos/89432/pexels-photo-89432.jpeg?h=350&dpr=2&auto=compress&cs=tinysrgb'},
-        {name: 'Category 10', image: 'https://images.pexels.com/photos/89432/pexels-photo-89432.jpeg?h=350&dpr=2&auto=compress&cs=tinysrgb'},
-        {name: 'Category 11', image: 'https://images.pexels.com/photos/89432/pexels-photo-89432.jpeg?h=350&dpr=2&auto=compress&cs=tinysrgb'},
-        {name: 'Category 12', image: 'https://images.pexels.com/photos/89432/pexels-photo-89432.jpeg?h=350&dpr=2&auto=compress&cs=tinysrgb'},
-        {name: 'Category 13', image: 'https://images.pexels.com/photos/89432/pexels-photo-89432.jpeg?h=350&dpr=2&auto=compress&cs=tinysrgb'},
-        {name: 'Category 14', image: 'https://images.pexels.com/photos/89432/pexels-photo-89432.jpeg?h=350&dpr=2&auto=compress&cs=tinysrgb'},
-      ];
+  {
+    name: 'Chinese',
+    image: 'https://images.pexels.com/photos/89432/pexels-photo-89432.jpeg?h=350&dpr=2&auto=compress&cs=tinysrgb',
+  },
+  {
+    name: 'Indian',
+    image: 'https://images.pexels.com/photos/89432/pexels-photo-89432.jpeg?h=350&dpr=2&auto=compress&cs=tinysrgb',
+  },
+  {
+    name: 'Italian',
+    image: 'https://images.pexels.com/photos/89432/pexels-photo-89432.jpeg?h=350&dpr=2&auto=compress&cs=tinysrgb',
+  },
+  {
+    name: 'French',
+    image: 'https://images.pexels.com/photos/89432/pexels-photo-89432.jpeg?h=350&dpr=2&auto=compress&cs=tinysrgb',
+  },
+  {
+    name: 'Spicy',
+    image: 'https://images.pexels.com/photos/89432/pexels-photo-89432.jpeg?h=350&dpr=2&auto=compress&cs=tinysrgb',
+  },
+  {
+    name: 'Polish',
+    image: 'https://images.pexels.com/photos/89432/pexels-photo-89432.jpeg?h=350&dpr=2&auto=compress&cs=tinysrgb',
+  },
+  {
+    name: 'German',
+    image: 'https://images.pexels.com/photos/89432/pexels-photo-89432.jpeg?h=350&dpr=2&auto=compress&cs=tinysrgb',
+  },
+  {
+    name: 'food',
+    image: 'https://images.pexels.com/photos/89432/pexels-photo-89432.jpeg?h=350&dpr=2&auto=compress&cs=tinysrgb',
+  },
+  {
+    name: 'Category 9',
+    image: 'https://images.pexels.com/photos/89432/pexels-photo-89432.jpeg?h=350&dpr=2&auto=compress&cs=tinysrgb',
+  },
+  {
+    name: 'Category 10',
+    image: 'https://images.pexels.com/photos/89432/pexels-photo-89432.jpeg?h=350&dpr=2&auto=compress&cs=tinysrgb',
+  },
+  {
+    name: 'Category 11',
+    image: 'https://images.pexels.com/photos/89432/pexels-photo-89432.jpeg?h=350&dpr=2&auto=compress&cs=tinysrgb',
+  },
+  {
+    name: 'Category 12',
+    image: 'https://images.pexels.com/photos/89432/pexels-photo-89432.jpeg?h=350&dpr=2&auto=compress&cs=tinysrgb',
+  },
+  {
+    name: 'Category 13',
+    image: 'https://images.pexels.com/photos/89432/pexels-photo-89432.jpeg?h=350&dpr=2&auto=compress&cs=tinysrgb',
+  },
+  {
+    name: 'Category 14',
+    image: 'https://images.pexels.com/photos/89432/pexels-photo-89432.jpeg?h=350&dpr=2&auto=compress&cs=tinysrgb',
+  },
+];
 
 
-class FoodCategories extends Component {
 
 
-  static navigationOptions = {
+export default class App extends Component {
+    static navigationOptions = {
     header: null,
   };
 
   constructor(props) {
+
     super(props);
-    this.state =  {
-      dataSource: new ListView.DataSource({
-        rowHasChanged: (r1, r2) => r1 !== r2
-      }).cloneWithRows(data)
-    };
+
+    this.state = {
+
+      isLoading: true,
+      text: '',
+    
+    }
+
+    this.arrayholder = [] ;
   }
+  
+  componentDidMount() {
+ 
+    return fetch('https://reactnativecode.000webhostapp.com/FruitsList.php')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+        this.setState({
+          isLoading: false,
+          dataSource: ds.cloneWithRows(responseJson),
+        }, function() {
 
+          // In this block you can do something with new state.
+          this.arrayholder = responseJson ;
 
-  render() {
-    const { onScroll = () => {} } = this.props;
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+      
+  }
+  
+  GetListViewItem (fruit_name) {
+    
+   Alert.alert(fruit_name);
+  
+  }
+  
+  SearchFilterFunction(text){
+     
+     const newData = this.arrayholder.filter(function(item){
+         const itemData = item.fruit_name.toUpperCase()
+         const textData = text.toUpperCase()
+         return itemData.indexOf(textData) > -1
+     })
+     this.setState({
+         dataSource: this.state.dataSource.cloneWithRows(newData),
+         text: text
+     })
+ }
+ 
+ ListViewItemSeparator = () => {
     return (
-      <ListView
-        ref="ListView"
-        style={styles.container}
-        dataSource={ this.state.dataSource }
+      <View
+        style={{
+          height: .5,
+          width: "100%",
+          backgroundColor: "#000",
+        }}
+      />
+    );
+  }
+ 
+ 
+ 
+  render() {
+    if (this.state.isLoading) {
+      return (
+        <View style={{flex: 1, paddingTop: 20}}>
+          <ActivityIndicator />
+        </View>
+      );
+    }
+ 
+    return (
+       <View style={styles.MainContainer}>
+       <View style={{flexDirection: 'row', flexWrap: 'wrap', marginTop: 15, marginBottom: 15, alignSelf:'center'}}>
+       <Icon containerStyle={{   backgroundColor : "#bab9bf"}}
+  name='search'/>
+       <TextInput 
+       style={styles.TextInputStyleClass}
+       onChangeText={(text) => this.SearchFilterFunction(text)}
+       value={this.state.text}
+       underlineColorAndroid='transparent'
+       placeholder="Search Here"
+        />
+        </View>
+ 
+        <ListView
+        contentContainerStyle={{  flexDirection: 'row', flexWrap: 'wrap', paddingLeft:4, paddingRight:4}}
+        dataSource={this.state.dataSource}
+        renderRow={rowData => (
+          <TouchableOpacity
+            style={{
+              // backgroundColor: 'pink',
+              width: '50%',
+              alignItems: 'center',
+              marginTop: 8,
+                              height: 130,
 
-        renderRow={(rowData) => (
-          <TouchableOpacity onPress={()=>{Alert.alert(rowData.name)}}>
-          <View key={rowData} style={ styles.rowContainer }>
-              <Image source={{ uri: rowData.image}} style={styles.photo} />
-            <Text style={ styles.rowText }>
-              { rowData.name }
-            </Text>
-          </View>
-          </TouchableOpacity>
-         )}
-
-
-        renderSeparator={(sectionId, rowId) => <View key={rowId} style={styles.separator} />}
-
-
-        renderScrollComponent={props => (
-          <ParallaxScrollView
-            onScroll={onScroll}
-
-            headerBackgroundColor="#fff"
-            stickyHeaderHeight={ STICKY_HEADER_HEIGHT }
-            parallaxHeaderHeight={ PARALLAX_HEADER_HEIGHT }
-            backgroundSpeed={10}
-
-
-            renderBackground={() => (
-              <View key="background">
-                <Image source={{uri: 'https://i.pinimg.com/736x/7b/ff/9f/7bff9fcfe8d530c7326a57ba66479b81--restaurant-branding-restaurant-interiors.jpg',
-                                width: window.width,
-                                height: PARALLAX_HEADER_HEIGHT}}/>
-                <View style={{position: 'absolute',
-                              top: 0,
-                              width: window.width,
-                              backgroundColor: 'rgba(0,0,0,0.4)',
-                              height: PARALLAX_HEADER_HEIGHT}}/>
-              </View>
-            )}
-
-
-            renderForeground={() => (
-              <View key="parallax-header" style={ styles.parallaxHeader }>
-                <Text style={ styles.Title }>
-                  Categories
-                </Text>
-              </View>
-            )}
-
-
+              // paddingBottom: 10,
+              paddingLeft: 3,
+              paddingRight: 3,
+            }}>
+            <Card
+              // dividerStyle={{ backgroundColor: 'pink' }}
+              containerStyle={{
+                backgroundColor: 'yellow',
+                height: '100%',
+                width: '100%',
+                marginTop: 0,
+              }}
+              image={{ uri: 'https://placeimg.com/640/480/tech' }}
+              imageStyle={{ backgroundColor: '#000',height: 130, }}
+              featuredTitle={rowData.fruit_name}
+              featuredTitleStyle={{ fontSize: 22, fontWeight: 'bold', }}
             />
+          </TouchableOpacity>
         )}
       />
+      
+ 
+      </View>
+      
     );
   }
 }
 
-const window = Dimensions.get('window');
-
-const AVATAR_SIZE = 120;
-const ROW_HEIGHT = 60;
-const PARALLAX_HEADER_HEIGHT = 150;
-const STICKY_HEADER_HEIGHT = 70;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'black',
-  },
-  background: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    width: window.width,
-    height: PARALLAX_HEADER_HEIGHT
-  },
-  stickySection: {
-        flex: 1,
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
+ 
+ MainContainer :{
 
+  justifyContent: 'center',
+  flex:1,
+  margin: 7,
+ 
   },
-  stickySectionText: {
-    color: 'white',
-    fontSize: 20,
-    margin: 10
+ 
+ rowViewContainer: {
+   fontSize: 17,
+   padding: 10
   },
-    photo: {
-    height: 40,
-    width: 40,
-    borderRadius: 20,
-  },
-  fixedSection: {
-    position: 'absolute',
-    bottom: 10,
-    right: 10
-  },
-  fixedSectionText: {
-    color: '#999',
-    fontSize: 20
-  },
-  parallaxHeader: {
-    alignItems: 'center',
-    flex: 1,
-    flexDirection: 'column',
-    paddingTop: 50
-  },
-  avatar: {
-    marginBottom: 10,
-    borderRadius: AVATAR_SIZE / 2
-  },
-  Title: {
-    color: 'white',
-    fontSize: 32,
-    paddingVertical: 5,
-  },
-  sectionTitleText: {
-    color: 'white',
-    fontSize: 18,
-    paddingVertical: 5
-  },
-  row: {
-    overflow: 'hidden',
-    paddingHorizontal: 10,
-    height: ROW_HEIGHT,
-    backgroundColor: 'white',
-    borderColor: '#ccc',
-    borderBottomWidth: 1,
-    justifyContent: 'center'
-  },
-  rowText: {
-    fontSize: 20,
-    marginLeft: 12,
 
-  },
-    rowContainer: {
-    flex: 1,
-    padding: 15,
-    flexDirection: 'row',
-    alignItems: 'center',
-    backgroundColor: '#F5F5F5',
-  },
-    separator: {
-    flex: 1,
-    height: StyleSheet.hairlineWidth,
-    backgroundColor: '#8E8E8E',
-  },
+  TextInputStyleClass:{
+  width:'90%',
+   textAlign: 'center',
+   height: 40,
+  // borderWidth: 1,
+   // borderColor: '#000',
+   // borderRadius: 1 ,
+   backgroundColor : "#bab9bf"
+        
+   }
+ 
 });
-
-export default FoodCategories;
